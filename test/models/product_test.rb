155132d7c0027376c11889273c61e35c91c7e15d
mutinhiri <barnabmutinhiri@gmail.com>
2022-03-03 21:58:21 +0200

    pedicate method for app testing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,5 +4,6 @@ class ProductTest < ActiveSupport::TestCase
   test "product attributes cannot be empty" do
     product = Product.new
     assert product.invalid?
+    assert product.errors[:title].any?
   end
 end
