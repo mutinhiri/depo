d7078d9bbd74ea23e8c3c4e07621bc8c1d772688
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-27 20:50:13 +0200

    Add null cart id

--- db/migrate/20220327184445_add_order_to_line_item.rb
+++ db/migrate/20220327184445_add_order_to_line_item.rb
@@ -1,5 +1,6 @@
 class AddOrderToLineItem < ActiveRecord::Migration[7.0]
   def change
-    add_reference :line_items, :order, null: false, foreign_key: true
+    add_reference :line_items, :order, null: true, foreign_key: true
+    change
   end
 end
