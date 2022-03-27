b9e6ab0562302311b26d756fce102cb56898100c
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-27 22:23:01 +0200

    Fix bug

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  has_many :line_items, dependent: destroy
+  has_many :line_items, dependent: :destroy
   enum pay_type: {
     "Check" => 0,
     "Credit card" => 1,
